53c148f64a08bce080312650811142d9337dd01b
Karim M. Ibrahim <karim.m.ibraheem@gmail.com>
2017-10-07 14:04:41 +0800

    initial commit

---
+++ Playground.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 13:24:17 2017
+
+@author: KarimM
+"""
+
+import json
+
+with open("Google Drive/PhD/Courses/Deep Learning/Project/train.json") as json_data:
+    d = json.load(json_data)
+
