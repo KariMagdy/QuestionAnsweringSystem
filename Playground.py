750e5d221fd7e170d4098001c011be84e5d3b749
Karim M. Ibrahim <karim.m.ibraheem@gmail.com>
2017-10-15 15:12:48 +0800

    Prepared embedding for context and query

--- Playground.py
+++ Playground.py
@@ -5,6 +5,7 @@ Created on Sat Sep 30 13:24:17 2017
 """
 from __future__ import print_function
 
+import os
 from keras.models import Sequential, Model
 from keras.layers.embeddings import Embedding
 from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
@@ -15,26 +16,121 @@ from functools import reduce
 import tarfile
 import numpy as np
 import re
+from tensorflow.python.platform import gfile
+from os.path import join as pjoin
 
-import json
-
-#with open("Google Drive/PhD/Courses/Deep Learning/Project/train.json") as json_data:
-#    d = json.load(json_data)
 
 dataDir = '/Users/KarimM/GoogleDrive/PhD/Courses/Deep_Learning/Project/data/squad/'
-MAX_SEQUENCE_LENGTH = 1000
+GLOVE_DIR = '/Users/KarimM/data/glove/'
 EMBEDDING_DIM = 100
+MAX_NB_WORDS = 20000
 
+
+def initialize_vocabulary(vocabulary_path):
+    # map vocab to word embeddings
+    if gfile.Exists(vocabulary_path):
+        rev_vocab = []
+        with gfile.GFile(vocabulary_path, mode="r") as f:
+            rev_vocab.extend(f.readlines())
+        rev_vocab = [line.strip('\n') for line in rev_vocab]
+        vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+    else:
+        raise ValueError("Vocabulary file %s not found.", vocabulary_path)
+        
+vocab, rev_vocab = initialize_vocabulary(pjoin(dataDir, "vocab.dat"))
+
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+
+# Reading the tekenized contexts
+contexts = []
 with open(dataDir + 'train.ids.context') as f:
-    contexts = f.readlines()
-
-with open(dataDir + 'train.ids.question') as f:
-    queries = f.readlines()
-  
-answerSpan = np.zeros(len(queries,2))
-with open(dataDir + 'train.span') as f:
     for line in f.readlines():
-        answerSpan = f.readlines()
+        contexts.append(map(int,line.split()))
+
+# Reading the tokenized queries
+queries = []
+with open(dataDir + 'train.ids.question') as f:
+    for line in f.readlines():
+        queries.append(map(int,line.split()))
+ 
+# Reading the answer spans 
+answerSpan = np.zeros([len(queries),2],dtype = int)
+with open(dataDir + 'train.span') as f:
+    for idx,line in enumerate(f.readlines()):
+        answerSpan[idx,:] = map(int , line.split())
     
 Max_Context_Length = max([len(i) for i in contexts])
 Max_Query_Length = max([len(i) for i in queries])
+
+print('Found %s contexts.' % len(contexts))
+
+
+contexts = pad_sequences(contexts, maxlen=Max_Context_Length)
+queries = pad_sequences(queries, maxlen=Max_Query_Length)
+
+
+print('Shape of context tensor:', contexts.shape)
+print('Shape of query tensor:', queries.shape)
+print('Shape of answers tensor:', answerSpan.shape)
+
+
+# prepare embedding matrix [[[Use all vocab????]]]
+num_words = min(MAX_NB_WORDS, len(vocab))
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+for word, i in vocab.items():
+    if i >= MAX_NB_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+        
+# load pre-trained word embeddings into an Embedding layer
+embedding_layer_Context = Embedding(num_words,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=Max_Context_Length,
+                            trainable=False)
+
+embedding_layer_Query = Embedding(num_words,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=Max_Query_Length,
+                            trainable=False)
+
+print('Training model.')
+
+# train a 1D convnet with global maxpooling
+Context_input = Input(shape=(Max_Context_Length,), dtype='int32')
+Query_input = Input(shape=(Max_Query_Length,), dtype='int32')
+embedded_Context = embedding_layer_Context(Context_input)
+embedded_Query = embedding_layer_Query(Query_input)
+
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(35)(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(labels_index), activation='softmax')(x)
+
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+model.fit(x_train, y_train,
+          batch_size=128,
+          epochs=10,
+validation_data=(x_val, y_val))
